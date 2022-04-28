82cfc92a68a04b796b043aa31a5e6a4682710286
kryton-me <80484001+kryton-me@users.noreply.github.com>
2022-04-28 20:26:12 +0100

    further tweaking and improvement

--- PyWinTTS.py
+++ PyWinTTS.py
@@ -1,59 +1,90 @@
+#!/bin/python3
 # -*- coding: utf-8 -*-
-import pyperclip
-import pyttsx
-import unicodedata
+
+#This software has an MIT Licence, and the libaries are as follows:
+import pyperclip as copyBuffer # BSD Licence
+import easy_pyttsx3 as tts # MIT licence
 
 class TextToSpeach:
 
 	def __init__(self):
+		
+		# Some magic numbers!
+		# I would sugest a config file but fear it would slow it down.
+		
+		# Add custom words, this has an electronics bias
+		self.customWords = {"€" = "Euros", 
+		"£" = 'Pounds',
+		"Ω" = 'Ohms',
+		"μ" = 'Mu'}
+		
+		# Pick your speed
+		self.speed = 120 # nice slow & clear.
+		
 		self.get_copy_buffer()
 		self.refine_text()
 		print(self.text)
 		self.say_it()
 
-		
 	def get_copy_buffer(self):
 		try:
-			self.data = pyperclip.paste()
+			
+			# extract text from the copy buffer, this libarty only uses text.
+			self.data = copyBuffer.paste()
+	
 		except:
-			self.data = "I did not read the copy buffer"
+			self.data = "Failed to ready copy buffer"
 
 	def refine_text(self):
-		if isinstance(self.data, basestring):
 		
-			# Attempt to convert the copy buffer ti UTF-8, does not work. 
-			self.data = unicode(self.data, 'utf-8')
+		for symbol, word in self.customWords :
+		
+			# convert a few known unicodes to words
+			self.data = self.data.replace(symbol, word)
 
-               # convert a few known unicodes to words
-			self.data = self.data.replace("€".decode('utf-8').encode('utf-8'), "Euros")
-			self.data = self.data.replace("£".decode('utf-8').encode('utf-8'), 'Pounds')
-			self.data = self.data.replace("Ω".decode('utf-8').encode('utf-8'), 'Ohms')
-			self.data = self.data.replace("Ω".decode('utf-8').encode('utf-8'), 'Ohms')
-			self.data = self.data.replace("μ".decode('utf-8').encode('utf-8'), 'Mu')
-
-			# Normalize the rest
+		# if your running windows
+		if platform.system() == "Windows" :
+		
+			# Normalize the text to ascii
 			self.text = unicodedata.normalize('NFKD', self.data).encode('ascii','ignore')
 
+		# if your running MacOS
+		elif platform.system() == "MacOS" :
+		
+			self.data.append(", also you should try the Native MacOS tool it's much better!")
+			
+			# Go the full utf-8!
 			self.text = self.data
 
-			# Depbug statment
-			#print("have cleaned text")
-
-		else:
-			
-			self.text = "No Text in copy buffer"
+		# if it's linux / solaris / BSD risk utf-8! this is full of future development.
+		else :
+		
+			# Go the full utf-8!
+			self.text = self.data
+		
+		# Depbug statment
+		print(self.text)
 
 	def say_it(self):
+	
 		# Set up the voise engine
-		engine = pyttsx.init()
-		# Slow it down a bit as you listing for accuracy not comprehension. (defult = 200)
-		engine.rate = 120
-		# load the text
-		engine.say(self.text)
-		# run it and hang around to confirm all is well.
-		engine.runAndWait()
-		# confirm it's over
+		engine = pyttsx3.init()
+		
+		# Set preffered listing speed.
+		engine.rate = self.speed
+		
+		try:
+		
+			# load the text
+			engine.say(self.text)
+		
+			# off you go.
+			engine.runAndWait()
+			
+		except:
+		
+			print("failed to get system to speak")
 		
 if __name__ == '__main__':
 
-	wibble = TextToSpeach()
+	TextToSpeach()
